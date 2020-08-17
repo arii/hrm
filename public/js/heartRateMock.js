bece8956409a5f122adf9a17c015cc73e0a7b96c
arii <anders.ariel@gmail.com>
2020-08-17 02:24:40 -0700

    heart rate mock

---
+++ public/js/heartRateMock.js
@@ -0,0 +1,28 @@
+ $(function () {
+const socket = io( {
+    transports:['websocket']
+});
+
+$("#sendMock").click( function() {
+    var name = $("#mockName").val();
+    var age = $("#mockAge").val();
+    var rate = $("#mockRate").val();
+    var device = $("#mockID").val();
+    var status_msg = $("#mockStatus").val();
+    var status_color = $("#mockStatusColor").val();
+    var hr = {
+            'name': name, 
+            'age': age,
+            'status_msg': status_msg,
+            'status_color': status_color,
+            'rate': rate,
+            'device': device,
+            }
+    socket.emit('hrm_client', hr);
+
+
+  });
+
+});
+
+
