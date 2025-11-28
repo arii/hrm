7db77662f371c148901c7bd1caaaef01a521e485
arii <anders.ariel@gmail.com>
2025-11-28 00:05:16 -0800

    chore: verification proof [skip ci]

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { Box, Typography } from '@mui/material'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 
 export default function Footer() {
   const currentYear = new Date().getFullYear()
