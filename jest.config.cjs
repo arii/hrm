c5a2159794c8c06049977477fe67b79cda59bfc8
arii <anders.ariel@gmail.com>
2025-12-01 21:39:12 -0800

    use standalone ci
    lint cache
    
    remove old jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,6 +5,21 @@ const config = {
   roots: ['<rootDir>/tests/unit'],
   testMatch: ['**/*.test.ts', '**/*.test.tsx'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  coverageDirectory: "coverage",
+  reporters : ["default", 
+    [
+      "jest-junit",
+      {
+        outputDirectory: "./test-results", // The directory where the XML file will be saved
+        outputName: "unit-results.xml", // The name of the JUnit XML file
+        suiteNameTemplate: "{filepath}", // Optional: customize the suite name
+        classNameTemplate: "{classname}", // Optional: customize the class name
+        titleTemplate: "{title}", // Optional: customize the test title
+      },
+    ],
+    
+  ],
+
   collectCoverageFrom: [
     'services/**/*.ts',
     'utils/socketManager.ts',
