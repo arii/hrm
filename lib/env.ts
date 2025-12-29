443283cdc135a6277c7c9819ea521e5e2ab6c005
Ariel Anders <anders.ariel@gmail.com>
2025-12-29 09:01:16 -0800

    Revert "fix: repair production startup command and address feedback"
    This reverts commit 1169b1add19db3d2156ab15a457d6701fc279128.

--- lib/env.ts
+++ lib/env.ts
@@ -25,7 +25,6 @@ const envSchema = z.object({
     .transform(Number),
   WEBSOCKET_GRACE_PERIOD_MS: z.string().default('5000').transform(Number),
   MAX_WS_CLIENTS: z.string().default('1000').transform(Number),
-  WS_MAX_CONNECTIONS: z.string().default('5').transform(Number),
 })
 
 export const env = envSchema.parse(process.env)
