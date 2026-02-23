fe1d33cddd6428ee14a4f0388db8cd07bde7cf92
Ariel Anders <anders.ariel@gmail.com>
2026-02-22 17:12:30 -0800

    Update page.tsx
    lint error

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import DashboardClient from '@/components/DashboardClient'
+  import DashboardClient from '@/components/DashboardClient'
 import { env } from '@/lib/env'
 
 interface DashboardProps {
