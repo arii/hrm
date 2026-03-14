e0657213035ec2d75b75b25e04ea4b0878fa64f2
Ariel Anders <anders.ariel@gmail.com>
2026-03-14 15:17:09 -0700

    Revert "test(vrt): fix flaky and failing component visual tests" (#9527)

--- context/LoadingContext.tsx
+++ context/LoadingContext.tsx
@@ -1,12 +1,6 @@
 'use client'
 
-import {
-  createContext,
-  useContext,
-  useState,
-  ReactNode,
-  useEffect,
-} from 'react'
+import { createContext, useContext, useState, ReactNode } from 'react'
 
 interface LoadingContextType {
   isLoading: boolean
@@ -17,16 +11,6 @@ const LoadingContext = createContext<LoadingContextType | undefined>(undefined)
 
 export const LoadingProvider = ({ children }: { children: ReactNode }) => {
   const [isLoading, setIsLoading] = useState(false)
-
-  useEffect(() => {
-    if (
-      typeof window !== 'undefined' &&
-      process.env.NEXT_PUBLIC_TESTING === 'true'
-    ) {
-      window.__TEST_CONTROLS__ = window.__TEST_CONTROLS__ || {}
-      window.__TEST_CONTROLS__.setIsLoading = setIsLoading
-    }
-  }, [])
 
   return (
     <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
